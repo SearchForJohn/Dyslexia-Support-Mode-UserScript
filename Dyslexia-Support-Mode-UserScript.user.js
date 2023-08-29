f09a218f649371b398a12b489667d31c19b1c6fc
Nanashi <johnnyjel1234567890@gmail.com>
2023-08-29 15:06:56 -0400

    Update Dyslexia-Support-Mode-UserScript.user.js

--- Dyslexia-Support-Mode-UserScript.user.js
+++ Dyslexia-Support-Mode-UserScript.user.js
@@ -9,6 +9,6 @@
 // ==/UserScript==
 
 Object.defineProperty(navigator, 'userAgent', {
-    value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 (dislexia)'
+    value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 (dyslexia)'
 })
   ;
