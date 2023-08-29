7945b2f60c70670b5fae2355c877869ee0e2264e
Nanashi <johnnyjel1234567890@gmail.com>
2023-08-29 15:31:43 -0400

    Update Dyslexia-Support-Mode-UserScript.user.js

--- Dyslexia-Support-Mode-UserScript.user.js
+++ Dyslexia-Support-Mode-UserScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Dyslexia-Support-Mode-UserScript
 // @description Dyslexia-Support-Mode-UserScript
-// @version         2.2
+// @version         2.2.1
 // @author          John Litzow
 // @installURL      https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
 // @updateURL       https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
@@ -10,6 +10,6 @@
 // ==/UserScript==
 
 Object.defineProperty(navigator, 'userAgent', {
-    value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 (dyslexia)'
+    value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 dyslexia'
 })
   ;
