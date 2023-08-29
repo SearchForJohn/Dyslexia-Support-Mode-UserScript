eae4af2c05906aedff5e983c48e0dc6dafdf92fe
Nanashi <johnnyjel1234567890@gmail.com>
2023-08-29 14:49:11 -0400

    Create Dyslexia-Support-Mode-UserScript.js

---
+++ Dyslexia-Support-Mode-UserScript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Dyslexia-Support-Mode-UserScript
+// @namespace   John Litzow
+// @description Dyslexia-Support-Mode-UserScript
+// @match       https://*searchforjohn.com*
+// @run-at      document-start
+// @grant       none
+// @version     1
+// ==/UserScript==
+
+Object.defineProperty(navigator, 'userAgent', {
+    value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 (dislexia)'
+})
+  ;
