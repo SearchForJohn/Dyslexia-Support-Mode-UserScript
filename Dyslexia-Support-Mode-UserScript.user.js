2d7e10a3e2751e1c265607b88a87a0ebf30be80e
Nanashi <johnnyjel1234567890@gmail.com>
2023-08-29 21:21:18 -0400

    Update Dyslexia-Support-Mode-UserScript.user.js

--- Dyslexia-Support-Mode-UserScript.user.js
+++ Dyslexia-Support-Mode-UserScript.user.js
@@ -3,7 +3,7 @@
 // @description Dyslexia-Support-Mode-UserScript
 // @version         4.0.0.0
 // @author          John Litzow
-// @installURL      https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
+// @downloadURL     https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
 // @updateURL       https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
 // @match           *://www.searchforjohn.com/*
 // @include         https://www.searchforjohn.com/*
@@ -13,3 +13,11 @@
 GM_registerMenuCommand("Enable Dyslexia Support.", setDyslexic, "d");
 
 GM_registerMenuCommand("Disable Dyslexia Support.", unSetDyslexic, "u");
+
+function setDyslexic() {
+  document.cookie = "dyslexic=true; SameSite=strict; expires=Fri, 31 Dec 9999 23:59:59 GMT"; console.log('Dyslexic');
+};
+function unSetDyslexic() {
+  document.cookie = "dyslexic=false; SameSite=strict; Max-Age=-99999999;";
+  console.log('UNDyslexic');
+};
