67c85d6ecfe4e2d142b938231f6e3269317a6fb4
Nanashi <johnnyjel1234567890@gmail.com>
2023-08-29 21:42:25 -0500

    Update Dyslexia-Support-Mode-UserScript.user.js

--- Dyslexia-Support-Mode-UserScript.user.js
+++ Dyslexia-Support-Mode-UserScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Dyslexia-Support-Mode-UserScript
 // @description Dyslexia-Support-Mode-UserScript
-// @version         4.0.1.0
+// @version         4.1.0.0
 // @author          John Litzow
 // @downloadURL     https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
 // @updateURL       https://searchforjohn.github.io/Dyslexia-Support-Mode-UserScript/Dyslexia-Support-Mode-UserScript.user.js
@@ -10,16 +10,42 @@
 // @grant           GM_registerMenuCommand
 // ==/UserScript==
 
+// Enable dyslexic mode by setting a cookie and reload the page
 function setDyslexic() {
-  document.cookie = "dyslexic=true; SameSite=strict; expires=Fri, 31 Dec 9999 23:59:59 GMT"; window.location.reload();
-};
-function unSetDyslexic() {
-  document.cookie = "dyslexic=false; SameSite=strict; Max-Age=-99999999;"; window.location.reload();
-};
-function reload() {
-  window.location.reload(true);
-};
+  try {
+    if (document.cookie.indexOf("dyslexic=true") === -1) {
+      document.cookie = "dyslexic=true; SameSite=strict; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+      alert("Dyslexia support mode enabled. Reloading page...");
+      window.location.reload();
+    }
+  } catch (e) {
+    console.error("Error enabling dyslexic mode:", e);
+  }
+}
 
+// Disable dyslexic mode by setting a cookie and reload the page
+function unSetDyslexic() {
+  try {
+    if (document.cookie.indexOf("dyslexic=false") === -1) {
+      document.cookie = "dyslexic=false; SameSite=strict; path=/; Max-Age=-99999999;";
+      alert("Dyslexia support mode disabled. Reloading page...");
+      window.location.reload();
+    }
+  } catch (e) {
+    console.error("Error disabling dyslexic mode:", e);
+  }
+}
+
+// Reload the current page
+function reload() {
+  try {
+    window.location.reload(true);
+  } catch (e) {
+    console.error("Error reloading the page:", e);
+  }
+}
+
+// Register the menu commands
 GM_registerMenuCommand("Enable Dyslexia Support.", setDyslexic, "d");
 GM_registerMenuCommand("Disable Dyslexia Support.", unSetDyslexic, "u");
 GM_registerMenuCommand("Reload.", reload, "r");
