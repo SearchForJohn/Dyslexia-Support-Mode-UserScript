5e0175cf934893c5eefeec6ec9a93a6659743b44
Nanashi <johnnyjel1234567890@gmail.com>
2023-08-29 20:42:25 -0400

    Update full.js

--- full.js
+++ full.js
@@ -16,14 +16,6 @@
 //    value: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 dyslexia'
 //});
 function setDyslexic() {
-    // Build the expiration date string:
-//    var expiration_date = new Date();
-//    var cookie_string = '';
-//    expiration_date.setFullYear(expiration_date.getFullYear() + 1);
-    // Build the set-cookie string:
-//cookie_string = "dyslexia = 1; path=/; expires=" + expiration_date.toUTCString();
-    // Create or update the cookie:
-//    document.cookie = cookie_string;
   GM_cookie.set({
     url: 'https://www.searchforjohn.com',
     name: 'dyslexia',
@@ -38,5 +30,3 @@ function unSetDyslexic() {
   GM_cookie.delete({ name: 'dyslexia' });
   console.log('UNDyslexic');
 };
-GM_registerMenuCommand("Enable Dyslexia Support.", setDyslexic, "d");
-GM_registerMenuCommand("Disable Dyslexia Support.", unSetDyslexic, "u");
